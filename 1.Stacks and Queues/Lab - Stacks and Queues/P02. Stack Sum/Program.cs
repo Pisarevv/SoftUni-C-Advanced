29f2141b1b076790de2c70f2d21df194b4c9bb4d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-17 19:00:10 +0300

    Task 2 complete

--- 1.Stacks and Queues/Lab - Stacks and Queues/P02. Stack Sum/Program.cs
+++ 1.Stacks and Queues/Lab - Stacks and Queues/P02. Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P02._Stack_Sum
 {
@@ -6,7 +8,42 @@ namespace P02._Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>();
+            string inputNumbers = Console.ReadLine();
+            string[] numbers = inputNumbers.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (string number in numbers)
+            {
+                stack.Push(int.Parse(number));
+            }
+            string command = Console.ReadLine().ToLower();
+
+            while ((command != "end"))
+            {
+                string[] cmdArgs = command.ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (cmdArgs[0] == "add")
+                {
+                    int n1 = int.Parse(cmdArgs[1]);
+                    int n2 = int.Parse(cmdArgs[2]);
+                    stack.Push(n1);
+                    stack.Push(n2);
+                }
+                else if (cmdArgs[0] == "remove")
+                {
+                    int countToRemove = int.Parse(cmdArgs[1]);
+                    if (stack.Count >= countToRemove)
+                    {
+                        for (int i = 0; i < countToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+            
+
+
         }
     }
 }
