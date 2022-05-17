f2aa7cd881319f73db2412e4603f33d868cee5e8
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-17 19:52:34 +0300

    Task 7

---
+++ 1.Stacks and Queues/Lab - Stacks and Queues/P07. Hot Potato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] players = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Queue<string> queue = new Queue<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            while ( queue.Count > 1)
+            {
+                for (int i = 1; i <= n-1; i++)
+                {
+                    string player = queue.Dequeue();
+                    queue.Enqueue(player);
+                }
+                string lostPlayer = queue.Dequeue();
+                Console.WriteLine($"Removed {lostPlayer}");
+            }
+            string lastPlayer = queue.Dequeue();
+            Console.WriteLine($"Last is {lastPlayer}");
+        }
+    }
+}
