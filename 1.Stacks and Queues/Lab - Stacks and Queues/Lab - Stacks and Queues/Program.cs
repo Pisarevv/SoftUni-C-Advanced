0732f6c34722718d45e6541eb842dd1f92553eb4
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-17 18:25:33 +0300

    Update

---
+++ 1.Stacks and Queues/Lab - Stacks and Queues/Lab - Stacks and Queues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab___Stacks_and_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in str)
+            {
+                stack.Push(c);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+        }
+    }
+}
