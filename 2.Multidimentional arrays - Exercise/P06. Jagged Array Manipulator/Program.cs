c85f7ea9af81d7a4720f0c0471e16d123aea6cf0
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-21 18:47:09 +0300

    Task 6 update

--- 2.Multidimentional arrays - Exercise/P06. Jagged Array Manipulator/Program.cs
+++ 2.Multidimentional arrays - Exercise/P06. Jagged Array Manipulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P06._Jagged_Array_Manipulator
 {
@@ -6,7 +7,68 @@ namespace P06._Jagged_Array_Manipulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rowsCount = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[rowsCount][];
+            FillJaggedMatrix(matrix);
+
+            for(int row = 0; row < rowsCount-1; row++)
+            {
+                if (matrix[row].Length == matrix[row + 1].Length)
+                {
+                    matrix[row] = matrix[row].Select(x => x*2).ToArray();
+                    matrix[row+1] = matrix[row+1].Select(x => x * 2).ToArray();
+                }
+                else
+                {
+                    matrix[row] = matrix[row].Select(x => x / 2).ToArray();
+                    matrix[row + 1] = matrix[row + 1].Select(x => x / 2).ToArray();
+                }
+            }
+
+            string command = string.Empty;
+
+            while((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string action = cmdArgs[0];
+                int row = int.Parse(cmdArgs[1]);
+                int col = int.Parse(cmdArgs[2]);
+                int value = int.Parse(cmdArgs[3]);
+
+                if(action == "Add")
+                {
+                    if (row >= 0 && row < matrix.GetLength(0) &&
+                        col >= 0 && col < matrix.Length)
+                    {
+                        matrix[row][col] =matrix[row][col] + value;
+                    }
+                }
+                else if (action == "Subtract")
+                {
+                    if (row >= 0 && row < matrix.GetLength(0) &&
+                        col >= 0 && col < matrix.Length)
+                    {
+                        matrix[row][col] = matrix[row][col] - value;
+                    }
+                }
+            }
+            foreach(int[] row in matrix)
+            {
+                Console.WriteLine(String.Join(" ", row));
+            }
         }
+
+        public static void FillJaggedMatrix (int[][] jaggedMatrix)
+        {
+            for (int row = 0; row < jaggedMatrix.GetLength(0) ; row++)
+            {
+                jaggedMatrix[row] = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+        }
+
+        
     }
 }
