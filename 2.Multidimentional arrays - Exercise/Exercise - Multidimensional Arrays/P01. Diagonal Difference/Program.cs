3b4e3ac4a0427b3be9fc3d8e65b9cecc3137bd30
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-21 09:37:44 +0300

    Task 1 complete

--- 2.Multidimentional arrays - Exercise/Exercise - Multidimensional Arrays/P01. Diagonal Difference/Program.cs
+++ 2.Multidimentional arrays - Exercise/Exercise - Multidimensional Arrays/P01. Diagonal Difference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P01._Diagonal_Difference
 {
@@ -6,7 +7,36 @@ namespace P01._Diagonal_Difference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            int rows = size;
+            int cols = size;
+
+            int[,] matrix = new int[rows, cols];
+            FillMatrix(matrix);
+
+            int sumPrimaryDiagonal = 0;
+            int sumSecondaryDiagonal = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                sumPrimaryDiagonal += matrix[i,i];
+               sumSecondaryDiagonal +=matrix[i,matrix.GetLength(0)-i-1];
+            }
+            int finalSum = sumPrimaryDiagonal-sumSecondaryDiagonal;
+            Console.WriteLine(Math.Abs(finalSum));
+            
+        }
+
+        private static void FillMatrix (int[,] numbers)
+        {
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    numbers[i, j] = input[j];
+                }
+            }
         }
     }
 }
