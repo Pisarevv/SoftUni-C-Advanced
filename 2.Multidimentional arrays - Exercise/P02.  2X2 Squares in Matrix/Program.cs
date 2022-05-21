3e2a9c4082e0eca1126ee9bbfdb24c58487d51f3
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-21 10:07:24 +0300

    Task 2 complete

--- 2.Multidimentional arrays - Exercise/P02.  2X2 Squares in Matrix/Program.cs
+++ 2.Multidimentional arrays - Exercise/P02.  2X2 Squares in Matrix/Program.cs
@@ -8,21 +8,35 @@ namespace P02.__2X2_Squares_in_Matrix
         static void Main(string[] args)
         {
             int[] sizeOfMatrix = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            int rows = sizeOfMatrix[0];
-            int cols = sizeOfMatrix[1];
+            int rowsCount = sizeOfMatrix[0];
+            int colsCount = sizeOfMatrix[1];
 
-            char[,] matrix = new char[rows, cols];
+            string[,] matrix = new string[rowsCount, colsCount];
             FillMatrix(matrix);
+            int SameMatrixes = 0;
+
+            for (int rows = 0; rows < rowsCount-1; rows++)
+            {
+                for (int cols = 0; cols < colsCount-1; cols++)
+                {
+                    if (matrix[rows, cols] == matrix[rows+1,cols] 
+                        && matrix[rows,cols+1] == matrix[rows + 1, cols + 1])
+                    {
+                        SameMatrixes++;
+                    }
+                }
+            }
+            Console.WriteLine(SameMatrixes);
             
         }
-        private static void FillMatrix(char[,] numbers)
+        private static void FillMatrix(string[,] numbers)
         {
-            for (int i = 0; i < numbers.GetLength(0); i++)
+            for (int row = 0; row < numbers.GetLength(0); row++)
             {
-                char[] rowData = Console.ReadLine().ToCharArray();
-                for (int j = 0; j < numbers.GetLength(1); j++)
+                string[] rowData = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < numbers.GetLength(1); col++)
                 {
-                    numbers[i, j] = rowData[j];
+                    numbers[row, col] = rowData[col];
                 }
             }
         }
