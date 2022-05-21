72e737ec6178d8a20aab3fd78901b9a854901a1d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-21 21:20:03 +0300

    Task 5 complete again

--- 2.Multidimentional arrays - Exercise/P05.V1.Snake Moves/Program.cs
+++ 2.Multidimentional arrays - Exercise/P05.V1.Snake Moves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P05.V1.Snake_Moves
 {
@@ -6,7 +7,55 @@ namespace P05.V1.Snake_Moves
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] n = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowCount = n[0];
+            int colCount = n[1];
+
+            char[,] matrix = new char[rowCount, colCount];
+            char[] snake = Console.ReadLine().ToCharArray();
+            int index = 0;
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                {
+                    if(row%2 == 0)
+                    {
+                        matrix[row, col] = snake[index];
+                        index++;
+
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+                    else
+                    {
+                        matrix[row, col] = snake[index];
+                        index++;
+
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+                    
+                }
+                PrintMatrix(matrix);
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
