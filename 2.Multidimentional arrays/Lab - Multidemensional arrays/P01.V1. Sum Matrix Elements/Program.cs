27b61029cc116dc51a4405e59f91753661c4a629
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-22 18:56:27 +0300

    Task 1 complete

--- 2.Multidimentional arrays/Lab - Multidemensional arrays/P01.V1. Sum Matrix Elements/Program.cs
+++ 2.Multidimentional arrays/Lab - Multidemensional arrays/P01.V1. Sum Matrix Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P01.V1._Sum_Matrix_Elements
 {
@@ -6,7 +7,25 @@ namespace P01.V1._Sum_Matrix_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] n = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[n[0],n[1]];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] values = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = values[col];
+                }
+            }
+            
+            int sum = 0;
+            foreach(int value in matrix)
+            {
+                sum += value;
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
         }
     }
 }
