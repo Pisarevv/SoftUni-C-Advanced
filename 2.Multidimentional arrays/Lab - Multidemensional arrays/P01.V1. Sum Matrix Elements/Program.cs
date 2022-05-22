299d3c6bc419cfb76d7ea9e74ec2f8db6c41ee50
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-22 18:51:07 +0300

    Task 1 V1 initialziation

---
+++ 2.Multidimentional arrays/Lab - Multidemensional arrays/P01.V1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.V1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
