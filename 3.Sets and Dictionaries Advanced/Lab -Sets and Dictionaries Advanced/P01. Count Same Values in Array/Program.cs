479e44709fed1a188839331a182441db1b23ab68
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-23 18:41:08 +0300

    Task 1,3 complete

---
+++ 3.Sets and Dictionaries Advanced/Lab -Sets and Dictionaries Advanced/P01. Count Same Values in Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            Dictionary<double,int> sameElements = new Dictionary<double,int>();
+
+            foreach(double element in array)
+            {
+                if (!sameElements.ContainsKey(element))
+                {
+                    sameElements.Add(element, 1);
+                }
+                else
+                {
+                    sameElements[element]++;
+                }
+            }
+            
+            foreach(var kvp in sameElements)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            } 
+        }
+    }
+}
---
+++ 3.Sets and Dictionaries Advanced/P03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace P03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).OrderByDescending(x => x).Take(3)
+                .ToArray();
+
+            Console.WriteLine(String.Join(" ", array));
+            /*int[] sortedArray = new int[array.Length];
+
+            sortedArray = array.OrderByDescending(x => x).ToArray();
+
+            if (sortedArray.Length > 2)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sortedArray[i] + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", sortedArray));
+            }
+            */
+        }
+    }
+}
