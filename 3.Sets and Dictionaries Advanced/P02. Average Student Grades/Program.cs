7f5c1c0ce142395dd5ca4a7d3cd2afd395dbf289
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-23 19:43:40 +0300

    Task 2 complete

--- 3.Sets and Dictionaries Advanced/P02. Average Student Grades/Program.cs
+++ 3.Sets and Dictionaries Advanced/P02. Average Student Grades/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P02._Average_Student_Grades
 {
@@ -6,7 +8,35 @@ namespace P02._Average_Student_Grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            var studentsGrades = new Dictionary<string,List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string studentName = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (!studentsGrades.ContainsKey(studentName))
+                {
+                    studentsGrades[studentName] = new List<double>();
+                }
+                studentsGrades[studentName].Add(grade);
+            }
+
+            foreach(var kvp in studentsGrades)
+            {
+
+                Console.Write($"{kvp.Key} - > ");
+                foreach(var grade in kvp.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.Write($"(avg: {kvp.Value.Average():F2})");
+                Console.WriteLine();
+            }
         }
     }
 }
