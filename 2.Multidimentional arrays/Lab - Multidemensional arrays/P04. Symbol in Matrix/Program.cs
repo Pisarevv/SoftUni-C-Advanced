f09a4e6d5b6921978de38fba1af173c3e7bd3b6f
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-23 13:54:49 +0300

    Task 5 v.1 Task 4 complete

--- 2.Multidimentional arrays/Lab - Multidemensional arrays/P04. Symbol in Matrix/Program.cs
+++ 2.Multidimentional arrays/Lab - Multidemensional arrays/P04. Symbol in Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P04._Symbol_in_Matrix
 {
@@ -6,7 +7,50 @@ namespace P04._Symbol_in_Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            FillMatrix(matrix);
+            char[] symbolToFind = Console.ReadLine().ToCharArray();
+            char symbol = symbolToFind[0];
+            int foundInRow = 0;
+            int foundInColumn = 0;
+            bool isFound = false;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        foundInRow = row;
+                        foundInColumn = col;
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"({foundInRow}, {foundInColumn})");
+
+            }
+            else
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+
+        }
+
+        private static void FillMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] values = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = values[col];
+                }
+            }
         }
     }
 }
---
+++ 2.Multidimentional arrays/Lab - Multidemensional arrays/P05.V1. Square with Maximum Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P05.V1._Square_with_Maximum_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
