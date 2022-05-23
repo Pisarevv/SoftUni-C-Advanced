611c5540795a11db4654364eb1a1f765e9f18f43
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-23 15:32:43 +0300

    Task 7 complete

--- 2.Multidimentional arrays/Lab - Multidemensional arrays/P07. Pascal Triangle/Program.cs
+++ 2.Multidimentional arrays/Lab - Multidemensional arrays/P07. Pascal Triangle/Program.cs
@@ -6,7 +6,27 @@ namespace P07._Pascal_Triangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[n][];
+            jaggedArray[0] = new int[1] { 1};
+            for (int i = 1; i < n; i++)
+            {
+                jaggedArray[i] = new int[i+1];
+                jaggedArray[i][0] = 1;
+                jaggedArray[i][jaggedArray[i].Length - 1] = 1;
+                for (int j = 1; j < jaggedArray[i].Length-1; j++)
+                {
+                    jaggedArray[i][j] = jaggedArray[i-1][j-1] + jaggedArray[i-1][j];
+                }
+                
+            }
+
+            foreach(int[] row in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ",row));
+            }
+
+           
         }
     }
 }
