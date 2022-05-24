9ca30c1e73ed9fe197a2a071e032777ff6c3e4ca
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-24 19:31:25 +0300

    Task 7 complete

---
+++ 3.Sets and Dictionaries Advanced/Lab -Sets and Dictionaries Advanced/P07. Parking Lot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> licensePlates = new HashSet<string>();
+            string input = string.Empty;
+
+            while((input = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = cmdArgs[0];
+                string plate = cmdArgs[1];
+                if (command == "IN")
+                {
+                    licensePlates.Add(plate);
+                }
+                else if (command == "OUT")
+                {
+                    licensePlates.Remove(plate);
+                }
+            }
+            foreach(var license in licensePlates)
+            {
+                Console.WriteLine(license);
+            }
+        }
+    }
+}
