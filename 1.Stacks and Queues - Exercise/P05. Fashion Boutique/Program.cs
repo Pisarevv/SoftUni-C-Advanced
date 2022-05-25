915d029fc39ee3a83460a7534c19f087f9eeefe9
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 14:31:52 +0300

    Task 5 complete

--- 1.Stacks and Queues - Exercise/P05. Fashion Boutique/Program.cs
+++ 1.Stacks and Queues - Exercise/P05. Fashion Boutique/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace P05._Fashion_Boutique
 {
@@ -6,7 +8,49 @@ namespace P05._Fashion_Boutique
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack stack = new Stack(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int rackCount = 0;
+            if (stack.Count > 0)
+            {
+                rackCount = 1;
+            }
+
+            int sumOfClothes = 0;
+
+            int clothesCount = stack.Count;
+
+            while (stack.Count > 0)
+            {
+               
+                sumOfClothes += (int)stack.Peek();
+                if (rackCapacity > sumOfClothes)
+                {
+                    stack.Pop();
+                }
+                else if (sumOfClothes == rackCapacity)
+                {
+                    if(stack.Count > 1)
+                    {
+                        rackCount++;
+                       
+                    }
+                    sumOfClothes = 0;
+                    stack.Pop();
+
+
+                }
+                else if (sumOfClothes > rackCapacity)
+                {
+                    rackCount++;
+                    sumOfClothes = 0;
+                }
+            }
+            Console.WriteLine(rackCount);
+
         }
     }
 }
