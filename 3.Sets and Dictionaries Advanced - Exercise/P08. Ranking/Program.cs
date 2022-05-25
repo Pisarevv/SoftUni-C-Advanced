dc35bd7787faea438287929faf805ccf80f00160
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 21:16:43 +0300

    Task 8 complete

--- 3.Sets and Dictionaries Advanced - Exercise/P08. Ranking/Program.cs
+++ 3.Sets and Dictionaries Advanced - Exercise/P08. Ranking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P08._Ranking
 {
@@ -6,7 +8,75 @@ namespace P08._Ranking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string,string> contests = new Dictionary<string,string>();
+            Dictionary<string,Dictionary<string,int>> participtains
+                = new Dictionary<string,Dictionary<string,int>>();
+
+            string input = string.Empty;
+            
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] cmdArgs = input
+                    .Split(':',StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string contest = cmdArgs[0];
+                string password = cmdArgs[1];
+                contests.Add(contest, password);
+            }
+
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                string[] cmdArgs = input
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string contest = cmdArgs[0];
+                string password = cmdArgs[1];
+                string username = cmdArgs[2];
+                int points = int.Parse(cmdArgs[3]);
+
+                if(contests.ContainsKey(contest) && contests[contest] == password)
+                {
+                    if (!participtains.ContainsKey(username))
+                    {
+                        participtains[username] = new Dictionary<string,int>();
+                    }
+                    if (!participtains[username].ContainsKey(contest))
+                    {
+                        participtains[username][contest] = 0;
+                    }
+                    if(points > participtains[username][contest])
+                    {
+                        participtains[username][contest] += points;
+                    }
+                   
+                }
+                
+            }
+
+            int higgestScore = 0;
+            string bestUser = string.Empty;
+
+            foreach (var user in participtains)
+            {
+                int currentMaxPoints = 0;
+                foreach(var data in user.Value)
+                {
+                    currentMaxPoints += data.Value; 
+                }
+                if (currentMaxPoints > higgestScore)
+                {
+                    higgestScore = currentMaxPoints;
+                    bestUser = user.Key;
+                }
+            }
+            Console.WriteLine($"Best candidate is {bestUser} with total {higgestScore} points.");
+            Console.WriteLine($"Ranking:");
+            foreach(var user in participtains.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{user.Key}");
+                foreach(var data in user.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"# {data.Key} -> {data.Value}");
+                }
+            }
         }
     }
 }
