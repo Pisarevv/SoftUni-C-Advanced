01f1157516dacc5c2000d65045afec515d834d01
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 19:38:45 +0300

    Task 3 complete

---
+++ 3.Sets and Dictionaries Advanced - Exercise/P03. Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                foreach(string element in input)
+                {
+                    elements.Add(element);
+                }
+            }
+            Console.WriteLine(string.Join(" ",elements));
+        }
+    }
+}
