fbd14303c0e033badcb5a36dec6a06231d93018b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 15:06:28 +0300

    Task 6 complete

--- 1.Stacks and Queues - Exercise/P06. Songs Queue/Program.cs
+++ 1.Stacks and Queues - Exercise/P06. Songs Queue/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P06._Songs_Queue
 {
@@ -6,7 +9,41 @@ namespace P06._Songs_Queue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> songPlaylist = new Queue<string>(Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries));
+
+            string input = string.Empty;
+
+            while (songPlaylist.Count > 0)
+            {
+                input = Console.ReadLine();              
+
+                if (input == "Play")
+                {              
+                        songPlaylist.Dequeue();
+                                 
+                }
+                else if (input.Contains("Add"))
+                {
+                    string songName = input.Remove(0, 4);
+
+                    if (songPlaylist.Contains(songName))
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                    else
+                    {
+                        songPlaylist.Enqueue(songName);
+                    }
+                }
+                else if (input == "Show")
+                {
+                    Console.WriteLine(string.Join(", ",songPlaylist));
+                }
+                
+            }
+            Console.WriteLine("No more songs!");
+
         }
     }
 }
