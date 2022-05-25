859d90ab4650d807ba367f5551f3dfe4818c971a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 12:10:26 +0300

    Task 1 complete

---
+++ 1.Stacks and Queues - Exercise/P01.Basic Stack Operations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace P01.Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack stack = new Stack();
+
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < input[0]; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < input[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(input[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if (!stack.Contains(input[2]))
+            {
+                int smallestElement = GetMinElementInStack(stack);
+                Console.WriteLine(smallestElement);
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+        }
+
+        private static int GetMinElementInStack (Stack stack)
+        {
+            int smallestElement = int.MaxValue;
+            foreach(int element in stack)
+            {
+                if (element < smallestElement)
+                {
+                    smallestElement = element;
+                }
+            }
+            return smallestElement;
+        }
+    }
+}
---
+++ 3.Sets and Dictionaries Advanced/P08. SoftUni Party/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputGuest = string.Empty;
+            var vipGuests = new HashSet<string>();
+            var regularGuests = new HashSet<string>();
+            while ((inputGuest = Console.ReadLine())!= "PARTY")
+            {
+                char firstChar = inputGuest[0];
+                if (Char.IsDigit(firstChar))
+                {
+                    vipGuests.Add(inputGuest);
+                }
+                else
+                {
+                    regularGuests.Add(inputGuest);
+                }
+            }
+
+            string guestVisited = string.Empty;
+            while ((guestVisited = Console.ReadLine())!= "END")
+            {
+                vipGuests.Remove(guestVisited);
+                regularGuests.Remove((guestVisited));
+            }
+            Console.WriteLine(vipGuests.Count+regularGuests.Count);
+            foreach(var vip in vipGuests)
+            {
+                Console.WriteLine(vip);
+            }
+            foreach(var regular in regularGuests)
+            {
+                Console.WriteLine(regular);
+            }
+            
+        }
+    }
+}
