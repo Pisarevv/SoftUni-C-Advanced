b699b09b551111ddbc9b967858032b121e224913
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 19:16:09 +0300

    Task 2 complete

--- 3.Sets and Dictionaries Advanced - Exercise/P02. Sets of Elements/Program.cs
+++ 3.Sets and Dictionaries Advanced - Exercise/P02. Sets of Elements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P02._Sets_of_Elements
 {
@@ -6,7 +8,32 @@ namespace P02._Sets_of_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+            List<int> matchingNumbers = new List<int>();
+            HashSet<int> firstSet = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> secondSet = new HashSet<int>();
+            for (int i = 0; i < m; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach(int number in firstSet)
+            {
+                if (secondSet.Contains(number))
+                {
+                    matchingNumbers.Add(number);
+                }
+            }
+            Console.WriteLine(string.Join(" ",matchingNumbers));
         }
     }
 }
