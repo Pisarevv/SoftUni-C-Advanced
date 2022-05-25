0ca5bbb3d9eb13e09171b97ac38c2895848cc881
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 19:51:14 +0300

    Task 4 update, task 5 complete

---
+++ 3.Sets and Dictionaries Advanced - Exercise/P05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char,int> symbols = new SortedDictionary<char,int>();
+
+            string input = Console.ReadLine();
+            char[] inputChar = input.ToCharArray();
+
+            foreach(char c in inputChar)
+            {
+                if (!symbols.ContainsKey(c))
+                {
+                    symbols[c] = 0;
+                }
+                symbols[c]++;
+            }
+
+            foreach(var c in symbols)
+            {
+                Console.WriteLine($"{c.Key} : {c.Value} time\\s");
+            }
+
+        }
+    }
+}
