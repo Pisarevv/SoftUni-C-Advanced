da9fba50fa885769bb79562bf1e2f79097ed44be
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-25 18:56:06 +0300

    Task 8 complete

---
+++ 1.Stacks and Queues - Exercise/P08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> openBrackets = new Stack<char>();
+            string input = Console.ReadLine();
+            bool areBalanced = false;
+            foreach (char bracket in input)
+            {
+                if (bracket == ('{') || bracket == '[' || bracket == '(')
+                {
+                    openBrackets.Push(bracket);
+                }
+                else if (bracket == ('}') || bracket == ']' || bracket == ')')
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        areBalanced = false;
+                        break;
+                    }
+                    char lastOpenBracket = openBrackets.Pop();
+
+                    if (lastOpenBracket == ('(') && bracket == (')'))
+                    {
+                        areBalanced = true;
+                    }
+                    else if (lastOpenBracket == ('[') && bracket == (']'))
+                    {
+                        areBalanced = true;
+                    }
+                    else if (lastOpenBracket == ('{') && bracket == ('}'))
+                    {
+                        areBalanced = true;
+                    }
+                    else
+                    {
+                        areBalanced = false;
+                    }
+                }
+            }
+
+            if (areBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+      
+           
+        }
+    }
+}
