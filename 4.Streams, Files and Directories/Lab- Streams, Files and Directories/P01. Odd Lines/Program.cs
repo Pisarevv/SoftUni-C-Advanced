f4440e9c0affa3a3104510b00b56f17e4cd2d378
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-26 19:01:56 +0300

    Task 1 complete

---
+++ 4.Streams, Files and Directories/Lab- Streams, Files and Directories/P01. Odd Lines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("input.txt");
+            using (reader)
+            {
+               
+               StreamWriter writer = new StreamWriter("output.txt");
+                using (writer)
+                {
+                    int lineNumber = 0;
+
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        if (lineNumber %2 == 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        lineNumber++;
+                    }
+                }
+
+            }
+        }
+    }
+}
