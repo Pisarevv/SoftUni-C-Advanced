a54ef528164f78f136fe5a225c09a25d9e1ead7d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-30 19:48:50 +0300

    Initialization,task 1 complete

---
+++ 5.Functional Programming/Lab-nFunctional Programming/P01. Sort Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace P01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                /*Console.WriteLine(string.Join
+                (", ", Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).Where(x => x%2 == 0).OrderBy(x => x)));*/
+
+            Func<string,int> parse = x => int.Parse(x);
+            Func<int,bool> isEven = x => x % 2 == 0;
+            Func<int,int> sort = x => x;
+
+            int[] numbers = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(parse).Where(isEven).OrderBy(sort).ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
