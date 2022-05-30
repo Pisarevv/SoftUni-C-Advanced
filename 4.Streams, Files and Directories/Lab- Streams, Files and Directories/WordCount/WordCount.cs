a9430dec252837007b575f7714ecf4586c376300
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-30 11:53:06 +0300

    Task 3 complete

--- 4.Streams, Files and Directories/Lab- Streams, Files and Directories/WordCount/WordCount.cs
+++ 4.Streams, Files and Directories/Lab- Streams, Files and Directories/WordCount/WordCount.cs
@@ -4,6 +4,8 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
+
     public class WordCount
     {
         static void Main(string[] args)
@@ -17,6 +19,41 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+           Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+           using (File.OpenRead(textFilePath))
+            {
+                string text = File.ReadAllText(textFilePath).ToString().ToLower();
+                string[] textForSearch = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+               
+
+                using (File.OpenRead(wordsFilePath))
+                {
+                    var words = File.ReadAllText(wordsFilePath).Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    
+                    foreach (var word in words)
+                    {
+                        string pattern = @$"\b{word}\b";
+                        Regex regex = new Regex(pattern);
+
+                        MatchCollection matchCollection = regex.Matches(text);
+                        if (!wordCounts.ContainsKey(word))
+                        {
+                            wordCounts[word] = matchCollection.Count;
+                        }
+                    }
+                    
+                }
+
+                
+            }
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in wordCounts.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
         }
     }
 }
