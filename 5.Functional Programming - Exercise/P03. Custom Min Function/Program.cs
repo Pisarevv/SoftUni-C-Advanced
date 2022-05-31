f8f1ab72e6eeadc1d5ad24a36a19b45b3366d494
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-31 18:46:09 +0300

    Task 3 complete

---
+++ 5.Functional Programming - Exercise/P03. Custom Min Function/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Func<List<int>, int> findSmallest = x => x.Min();
+
+            Console.WriteLine(findSmallest(numbers));
+        }
+    }
+}
