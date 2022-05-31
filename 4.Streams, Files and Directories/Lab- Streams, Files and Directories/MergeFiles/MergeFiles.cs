6deab5df2fe8837bfafd7149f382d4a165ff2fcc
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-31 13:47:06 +0300

    task 4 complete

--- 4.Streams, Files and Directories/Lab- Streams, Files and Directories/MergeFiles/MergeFiles.cs
+++ 4.Streams, Files and Directories/Lab- Streams, Files and Directories/MergeFiles/MergeFiles.cs
@@ -1,7 +1,10 @@
 ﻿namespace MergeFiles
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+
     public class MergeFiles
     {
         static void Main(string[] args)
@@ -15,7 +18,37 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
-            
+            using (StreamReader reader1 = new StreamReader(firstInputFilePath))
+            {
+                using (StreamReader reader2 = new StreamReader(secondInputFilePath))
+                {
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    { 
+                       
+                        Queue<string> firstFileQueue = new Queue<string>(reader1.ReadToEnd().Split('\n',StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()));
+                        Queue<string> secondFileQueue = new Queue<string>(reader2.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()));
+                        int counter = 0;
+                        while (true)
+                        {
+                            if (counter % 2 == 0 && firstFileQueue.Count > 0)
+                            {
+                                writer.Write(firstFileQueue.Dequeue() + "\n");
+                            }
+                            if (counter % 2 != 0 && secondFileQueue.Count > 0)
+                            {
+                                writer.Write(secondFileQueue.Dequeue() + "\n");
+                            }
+                            counter++;
+                            if (firstFileQueue.Count == 0 && secondFileQueue.Count == 0)
+                            {
+                                break;
+                            }
+                        }
+                        
+                        
+                    }
+                }
+            }
         }
     }
 }
