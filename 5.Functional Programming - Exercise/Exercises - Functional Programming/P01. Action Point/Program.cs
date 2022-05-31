9a7c16fa6a289532a8d302ed0aa6c2644ee6a86a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-31 18:21:42 +0300

    Task 1 complete

---
+++ 5.Functional Programming - Exercise/Exercises - Functional Programming/P01. Action Point/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printNames = x => Console.WriteLine(x);
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            names.ForEach(printNames);
+        }
+
+       
+    }
+}
