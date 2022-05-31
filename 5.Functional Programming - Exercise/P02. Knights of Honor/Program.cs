366323441eed25a1323fa1034557f87b96574271
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-05-31 18:35:45 +0300

    Task 2 comp;ete

---
+++ 5.Functional Programming - Exercise/P02. Knights of Honor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,string> addSir = name => "Sir " + name;
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(addSir(name));
+            }
+        }
+    }
+}
