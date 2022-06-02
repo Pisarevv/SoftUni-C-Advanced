bd6d9aa1c3ab0d8e3878b60a228c90bb81851247
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-02 20:29:54 +0300

    task 1 complete

---
+++ 6.Defining Classes - Exercise/Exercise - Defining classes/DefiningClasses/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class Person
+    {
+        //fields - полета - характеристики !private!
+        private string name;
+        private int age;
+
+        //constructor
+        public Person() // По подразбиране
+        {
+
+        }
+
+        public Person(string name, int age) // с параметри
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //methods
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
---
+++ 6.Defining Classes - Exercise/Exercise - Defining classes/DefiningClasses/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 20;
+            Person person1 = new Person("George",18);
+            Person person2 = new Person("Jose", 43);
+        }
+    }
+}
