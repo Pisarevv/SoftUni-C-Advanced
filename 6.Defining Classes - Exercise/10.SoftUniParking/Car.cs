d12ce185341e018676bfa87fca0d4f877247c4de
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-05 16:27:29 +0300

    task 10 complete

---
+++ 6.Defining Classes - Exercise/10.SoftUniParking/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    internal class Car
+    {
+        //fields
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+        //fields
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+        //ctor 
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Make: {make}" + Environment.NewLine);
+            sb.Append($"Model: {model}" + Environment.NewLine);
+            sb.Append($"HorsePower: {horsePower}" + Environment.NewLine);
+            sb.Append($"RegistrationNumber: {registrationNumber}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 6.Defining Classes - Exercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    internal class Parking
+    {
+        //fields
+        private List<Car> cars;
+        private int capacity;
+        //properties
+        public List<Car> Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        public Parking(int capacity)
+        {
+            Cars = new List<Car>();
+            Capacity = capacity;
+        }
+        public int Count { get { return cars.Count; } }
+        //methods
+
+        public string AddCar(Car car)
+        {
+            if (cars.Contains(car))
+            {
+               return ("Car with that registration number, already exists!");
+            }
+            else if (cars.Count >= capacity)
+            {
+                return ("Parking is full!");
+            }
+            else
+            {
+                cars.Add(car);
+                return ($"Successfully added new car {car.Make} {car.RegistrationNumber}");
+            }
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if(cars.Exists(x => x.RegistrationNumber == registrationNumber))
+            {
+                cars.RemoveAll((x => x.RegistrationNumber == registrationNumber));
+                return($"Successfully removed {registrationNumber}");
+            }
+            else
+            {
+                return ("Car with that registration number, doesn't exist!");
+            }
+        }
+        
+        public Car GetCar(string registrationNumber)
+        {
+            return cars.Find(x => x.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                cars.RemoveAll(x => x.RegistrationNumber == registrationNumber);
+            }
+            
+        }
+
+       
+
+        
+
+    }
+}
