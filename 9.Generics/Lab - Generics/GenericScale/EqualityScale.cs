da7c3e000930010ca0800aaf36a8697fa8669a39
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-12 22:43:24 +0300

    Task 3 complete

---
+++ 9.Generics/Lab - Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public  class EqualityScale<T>
+    {
+        public static bool AreEqual(T left, T right)
+        {
+            return left.Equals(right);
+        }
+    }
+}
---
+++ 9.Generics/Lab - Generics/GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>();
+            var equals = EqualityScale<int>.AreEqual(1,1); 
+        }
+    }
+}
