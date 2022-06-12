87cb1a15494af542987ceafb3e551b5617dcf76e
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-12 23:05:24 +0300

    Task 1 and 2 complete

---
+++ 9.Generics - Exercise/Exercise - Generics/Generics/Generic Box of String/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Box_of_String
+{
+    internal class Box<T>
+    {
+        public T data { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{data.GetType()}: {data}");
+            return sb.ToString();
+        }
+    }
+}
