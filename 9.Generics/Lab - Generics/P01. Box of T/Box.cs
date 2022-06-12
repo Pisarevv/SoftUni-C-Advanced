9dd074e5798d90c615beb375afa17a1d49ed76fb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-12 22:03:26 +0300

    Task 1 complete

---
+++ 9.Generics/Lab - Generics/P01. Box of T/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01._Box_of_T
+{
+    public class Box<T>
+    {
+        private List<T> data;
+        public int Count { get { return this.data.Count; } }
+        public Box()
+        {
+            data = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+            if (this.data.Count == 0)
+            {
+                throw new InvalidOperationException("The box is empty");
+            }
+            var lastElement = this.data[this.data.Count - 1];
+            this.data.RemoveAt(this.data.Count - 1);
+            return lastElement;
+        }
+
+        
+       
+    }
+}
---
+++ 9.Generics/Lab - Generics/P01. Box of T/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01._Box_of_T
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
