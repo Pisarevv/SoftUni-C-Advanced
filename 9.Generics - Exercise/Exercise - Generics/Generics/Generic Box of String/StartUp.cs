60b9754a4ceb7df5299533a36b0e4c9e3f770f25
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-13 23:35:16 +0300

    task 8 complete

--- 9.Generics - Exercise/Exercise - Generics/Generics/Generic Box of String/StartUp.cs
+++ 9.Generics - Exercise/Exercise - Generics/Generics/Generic Box of String/StartUp.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Generics
+namespace Genereics
 {
     internal class StartUp
     {
@@ -33,14 +33,26 @@ namespace Generics
             int count = box.CountOfGreaterElements(list, elementToCompare);
             Console.WriteLine(count);*/
             string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
-            Tuple<string, string> firstTurple = new Tuple<string, string>(input[0]+ " " + input[1],input[2]);
+            if(input.Length > 4)
+            {
+                input[3] = input[3] + " " + input[4];
+            }
+            Tuple<string, string, string> firstTurple = new Tuple<string, string, string>(input[0]+ " " + input[1],input[2], input[3]);
             string[] input2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-            Tuple<string, int> secondTurple = new Tuple<string, int>(input2[0], int.Parse(input2[1]));
+            if (input2[2] == "drunk")
+            {
+                input2[2] = "True";
+            }
+            else
+            {
+                input2[2] = "False";
+            }
+            Tuple<string, int, string> secondTurple = new Tuple<string, int, string>(input2[0], int.Parse(input2[1]),input2[2]);     
             string[] input3 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-            Tuple<int, double> thirdTurple = new Tuple<int, double>(int.Parse(input3[0]), double.Parse(input3[1]));
-            Console.WriteLine($"{firstTurple.Item1} -> {firstTurple.Item2}");
-            Console.WriteLine($"{secondTurple.Item1} -> {secondTurple.Item2}");
-            Console.WriteLine($"{thirdTurple.Item1} -> {thirdTurple.Item2}");
+            Tuple<string, double, string> thirdTurple = new Tuple<string, double,string>(input3[0], double.Parse(input3[1]),input3[2]);
+            Console.WriteLine($"{firstTurple.Item1} -> {firstTurple.Item2} -> {firstTurple.Item3}");
+            Console.WriteLine($"{secondTurple.Item1} -> {secondTurple.Item2} -> {secondTurple.Item3}");
+            Console.WriteLine($"{thirdTurple.Item1} -> {thirdTurple.Item2} -> {thirdTurple.Item3}");
 
 
         }
