5d624cbe69d540711fd604dd670fe2970d2d8057
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-14 19:45:47 +0300

    Task 1 complete

---
+++ 10.Iterators and Comparators - Exercise/Exercise - Iterators and Comparators/P01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P01._ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            this.collection = new List<T>(data);
+            this.currIndex = 0;
+        }
+
+        public bool HasNext()
+        {
+            return currIndex < this.collection.Count-1;
+        }
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation");
+            }
+            Console.WriteLine(collection[currIndex]);
+        }
+    }
+}
