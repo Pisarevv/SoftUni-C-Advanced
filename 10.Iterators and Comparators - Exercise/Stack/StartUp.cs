8d87fb3a3492433e9d018b09358f33fb16145edb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-14 20:06:27 +0300

    task 3 complete

---
+++ 10.Iterators and Comparators - Exercise/Stack/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            while (true)
+            {
+                var tokens = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "END")
+                {
+                    break;
+                }
+                if (tokens[0] == "Push")
+                {
+                    stack.Push(tokens.Skip(1).Select(e => e.Split(",").First()).ToArray());
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+            }
+
+            foreach (var token in stack)
+            {
+                Console.WriteLine(token);
+            }
+            foreach (var token in stack)
+            {
+                Console.WriteLine(token);
+            }
+        }
+    }
+}
