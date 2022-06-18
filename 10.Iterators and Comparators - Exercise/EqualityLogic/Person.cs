a274e11f2abc0ecea98d706d9cde69b36757a96d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-18 14:53:26 +0300

    task 6 complete

---
+++ 10.Iterators and Comparators - Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EqualityLogic
+{
+    internal class Person: IComparable<Person>
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Age.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            var other = obj as Person;
+            bool result = false;
+            if (other == null)
+            {
+                return false;
+            }
+            return age == other.Age && name == other.Name;
+        }
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+    }
+}
---
+++ 10.Iterators and Comparators - Exercise/EqualityLogic/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EqualityLogic
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var person = new Person(cmdArgs[0], int.Parse(cmdArgs[1]));
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
