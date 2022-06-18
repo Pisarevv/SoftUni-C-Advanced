aa880f12abd04dfd8e3371caa02ea87d57842c91
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-18 12:57:03 +0300

    Task 4 complete

---
+++ 10.Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    internal class Lake : IEnumerable<int>
+    {
+        private List<int> list;
+        public Lake(params int[] data)
+        {
+            this.List = new List<int>(data);
+        }
+        public List<int> List { get => list; set => list = value; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.list.Count; i++)
+            {
+                if (list[i] % 2 != 0)
+                {
+                    yield return list[i];
+                }
+            }
+
+            for (int i = this.list.Count - 1; i >= 0; i--)
+            {
+                if (list[i] % 2 == 0)
+                {
+                    yield return list[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 10.Iterators and Comparators - Exercise/Froggy/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var Lake = new Lake(Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Console.WriteLine(string.Join(", ", Lake));
+     
+        }
+    }
+}
