a3d72f7ced4b1867fe6f08fa33c1aae81dbe237c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-18 14:50:49 +0300

    Task 5 complete

---
+++ 10.Iterators and Comparators - Exercise/Comparing Objects/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    internal class Person : IComparable<Person>
+    {
+        //field
+        private string name;
+        private int age;
+        private string town;
+        //ctor
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+
+
+        //prop
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+        public int CompareTo(Person other)
+        {
+            int result = 0;
+            if (this.Name == other.Name && this.Age == other.Age && this.Town == other.Town)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = -1;
+            }
+            return result;
+        }
+    }
+}
---
+++ 10.Iterators and Comparators - Exercise/Comparing Objects/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            List<Person> people = new List<Person>();
+            while((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command.Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+                Person newPerson = new Person(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
+                people.Add(newPerson);
+            }
+            int n = int.Parse(Console.ReadLine());
+            Person toFind = people[n-1];
+
+            int matches = 0;
+            int notEqual = 0;
+            foreach(Person person in people)
+            {
+                int result = person.CompareTo(toFind);
+                if(result == 0)
+                {
+                    matches++;
+                }
+                if(result == -1)
+                {
+                    notEqual++;
+                }
+            }
+            if (matches > 1)
+            {
+                Console.WriteLine($"{matches} {notEqual} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+            
+        }
+    }
+}
