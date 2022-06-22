209253e35f7809624bee0519f7095091e228a19f
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-22 18:44:49 +0300

    task 3 complete

---
+++ Exam preparation/C# Advanced Retake Exam - 13 April 2022/Skeleton/Zoo/Animal.cs
@@ -0,0 +1,28 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        private string species;
+        private string diet;
+        private double weight;
+        private double length;
+
+        public Animal(string species, string diet, double weight, double length)
+        {
+            this.Species = species;
+            this.Diet = diet;
+            this.Weight = weight;
+            this.Length = length;
+        }
+
+        public string Species { get => species; set => species = value; }
+        public string Diet { get => diet; set => diet = value; }
+        public double Weight { get => weight; set => weight = value; }
+        public double Length { get => length; set => length = value; }
+
+        public override string ToString()
+        {
+            return $"The {this.Species} is a {this.Diet} and weighs {this.Weight} kg.";
+        }
+    }
+}
---
+++ Exam preparation/C# Advanced Retake Exam - 13 April 2022/Skeleton/Zoo/Zoo.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+namespace Zoo
+{
+    public class Zoo
+    {
+        private List<Animal> animals;
+        private string name;
+        private int capacity;
+
+        public Zoo(string name, int capacity)
+        {
+            this.name = name;
+            this.capacity = capacity;
+            this.animals = new List<Animal>();
+        }
+
+        public List<Animal> Animals { get => animals; set => animals = value; }
+        public string Name { get => name; set => name = value; }
+        public int Capacity { get => capacity; set => capacity = value; }
+
+        public string AddAnimal(Animal animal)
+        {
+            if (string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+            if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+            if (this.Animals.Count == Capacity)
+            {
+                return "The zoo is full.";
+            }
+            else
+            {
+                this.Animals.Add(animal);
+                return $"Successfully added {animal.Species} to the zoo.";
+            }
+        }
+        public int RemoveAnimals(string species)
+        {
+            int count = this.Animals.RemoveAll(a => a.Species == species);
+            return count;
+        }
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            List<Animal> filteredAnimals = Animals.FindAll(x => x.Diet == diet);
+            return filteredAnimals;
+        }
+        public Animal GetAnimalByWeight(double weight)
+        {
+            return Animals.Find(x => x.Weight == weight);
+        }
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            int count = 0;
+            foreach (var animal in this.Animals)
+            {
+                if(animal.Length >= minimumLength && animal.Length <= maximumLength)
+                {
+                    count++;
+                }
+            }
+            return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
