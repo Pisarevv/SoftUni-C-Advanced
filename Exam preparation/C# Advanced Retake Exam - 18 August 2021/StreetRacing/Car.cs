01766496314921c24aa230a77c8343cd2e45b8fc
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-06-23 15:26:33 +0300

    task 3 complete

---
+++ Exam preparation/C# Advanced Retake Exam - 18 August 2021/StreetRacing/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private string licensePlate;
+        private int horsePower;
+        private double weight;
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.make = make;
+            this.model = model;
+            this.licensePlate = licensePlate;
+            this.horsePower = horsePower;
+            this.weight = weight;
+        }
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public string LicensePlate { get => licensePlate; set => licensePlate = value; }
+        public int HorsePower { get => horsePower; set => horsePower = value; }
+        public double Weight { get => weight; set => weight = value; }
+
+        public override string ToString()
+        {
+            return $"Make: {this.Make}" + Environment.NewLine +
+                $"Model: {this.Model}" + Environment.NewLine +
+                $"License Plate: {this.LicensePlate}" + Environment.NewLine +
+                $"Horse Power: {this.HorsePower}" + Environment.NewLine +
+                $"Weight: {this.Weight}";
+
+        }
+    }
+}
---
+++ Exam preparation/C# Advanced Retake Exam - 18 August 2021/StreetRacing/Race.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace StreetRacing
+{
+    public class Race
+    {
+        private List<Car> cars;
+        private string name;
+        private string type;
+        private int laps;
+        private int capacity;
+        private int maxHorsePower;
+
+        public Race(string name,string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+            this.Cars = new List<Car>();
+        }
+
+        public List<Car> Cars { get => cars; set => cars = value; }
+        public string Name { get => name; set => name = value; }
+        public int Laps { get => laps; set => laps = value; }
+        public int Capacity { get => capacity; set => capacity = value; }
+        public int MaxHorsePower { get => maxHorsePower; set => maxHorsePower = value; }
+        public string Type { get => type; set => type = value; }
+
+        public int Count => this.cars.Count;
+        public void Add(Car car)
+        {
+            bool foundCar = cars.Any(x => x.LicensePlate == car.LicensePlate);
+            if (cars.Count < this.Capacity && !foundCar && car.HorsePower <= MaxHorsePower)
+            {
+                cars.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            int count = cars.RemoveAll(x => x.LicensePlate == licensePlate);
+            return count > 0;
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            return cars.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        }
+        public Car GetMostPowerfulCar()
+        {
+            if (cars.Count == 0)
+            {
+                return null;
+            }
+            return cars.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {this.Name} - Type: {this.Type} (Laps: {this.Laps})");
+            foreach(var car in this.Cars)
+            {
+                sb.AppendLine(car.ToString().Trim());
+            }
+            return sb.ToString();
+        }
+    }
+}
